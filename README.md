dc117bad52527a67fdca51907fce48c0effa167a
antonythilagar <antony.thilagar@nciportal.com>
2017-01-28 14:04:29 +0530

    Add node app

--- README.md
+++
@@ -1,4 +0,0 @@
-# project1
-Hello World..!
-I am Antony...
-Thank You...!
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+ 
 var app = express();
-
-app.get('/',function(req,res) {
-  res.send("Hello World");
+ 
+app.get('/', function (req, res) {
+  res.send('hello world');
 });
-
-app.listen(4000);
+ 
+app.listen(process.env.PORT || 2002);
+ 
+module.exports = app;
